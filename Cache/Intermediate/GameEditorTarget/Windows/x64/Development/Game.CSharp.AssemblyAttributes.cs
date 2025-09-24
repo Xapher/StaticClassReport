7587829852d38ca14c1dc7c9a48a6c149ddce4da
Xapher <schreckengostb1@gmail.com>
2025-09-23 18:18:04 -0700

    initial

---
+++ Cache/Intermediate/GameEditorTarget/Windows/x64/Development/Game.CSharp.AssemblyAttributes.cs
@@ -0,0 +1,1 @@
+﻿[assembly: global::System.Runtime.Versioning.TargetFrameworkAttribute(".NETCoreApp,Version=v9.0", FrameworkDisplayName = ".NET 9.0")]
---
+++ Source/Game/SimpleActorStaticClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// SimpleActorStaticClass Script.
+/// Holds a single instence of an actor and it's called count to provide evidence for non-disposed static classes
+/// </summary>
+public static class SimpleActorStaticClass 
+{
+    static Actor actorReference;
+
+    static int calledCount = 0;
+
+    static SimpleActorStaticClass()
+    {
+        Debug.Log(actorReference);
+    }
+
+    public static void setActor(Actor a)
+    {
+        Debug.Log("Call Count: " + calledCount + " --------------------------------------- ");
+        Debug.Log(actorReference);
+        actorReference = a;
+        calledCount++;
+        Debug.Log("Call Count: " + calledCount + " --------------------------------------- ");
+        Debug.Log(actorReference);
+    }
+}
